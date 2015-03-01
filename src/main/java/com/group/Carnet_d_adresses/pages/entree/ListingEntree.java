77499ec888df2c3a23189299cc09209799848439
Marsollier-IUT-info <thomas.marsollier@etu.univ-orleans.fr>
2015-03-01 20:13:14 +0100

    Ajout des fonctionnalites "Vue", "Edition" et "Suppression".

--- src/main/java/com/group/Carnet_d_adresses/pages/entree/ListingEntree.java
+++ src/main/java/com/group/Carnet_d_adresses/pages/entree/ListingEntree.java
@@ -2,11 +2,25 @@ package com.group.Carnet_d_adresses.pages.entree;
 
 import java.util.ArrayList;
 
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.grid.GridDataSource;
+
 import com.group.Carnet_d_adresses.entities.Entree;
 import com.group.Carnet_d_adresses.entities.InstanceCarnetDAdresses;
 
 public class ListingEntree
 {
+	@Property
+	private Entree currentEntree;
+	
+	@InjectComponent
+    private Zone result;
+	
+	@Property
+    private GridDataSource source;
+	
 	public ArrayList<Entree>getEntrees()
 	{		
 		/** - getEntrees() -> On recupere les entrees
@@ -14,4 +28,9 @@ public class ListingEntree
 		 *  - InstanceCarnetDAdresses -> stocke dans la classe "InstanceCarnetDAdresses". */
 		return InstanceCarnetDAdresses.getCarnetDAdresses().getEntrees();
 	}
+	
+	Object onAction()
+    {
+        return result.getBody();
+    }
 }
